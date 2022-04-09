0755d2dd1cd7f43898cbea7a6b598a46db597f25
Fikri Ariqsya <fikriariqsyarahman98@gmail.com>
2022-04-09 15:58:33 +0700

    Program Finished

--- lib/play.dart
+++ lib/play.dart
@@ -44,14 +44,40 @@ class _PlayMusicState extends State<PlayMusic> {
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Container(
-            height: 400,
+            height: 550,
             width: double.infinity,
-            child: Text(
-              'Play Audio',
-              style: TextStyle(fontSize: 35, color: Colors.white),
+            child: SingleChildScrollView(
+              child: Text(
+                '''
+
+
+                                      MALINKUNDANG
+
+    Hidup ibu dan anak ini serba kekurangan. Meski begitu, sang ibu selalu berusaha keras untuk memberikan kehidupan yang layak untuk anak laki-lakinya.
+Ketika Malin beranjak dewasa, dia pergi merantai bersama seorang saudagar kaya. Ia pun berjanji akan pulang dan menjemput ibunya setelah kaya raya.
+"Malin akan pulang setelah berhasil. Malin akan menjemput ibu. Doakan Malin ya," katanya pada sang ibu sebelum pergi.
+
+    Bertahun-tahun kemudian, Malin Kundang berhasil menjadi pedagang yang kaya. Ia pun menikah dengan putri seorang kepala kampung. Sayangnya, kehidupannya yang bergelimang harta membuat Malin lupa dengan sang ibu. Ia malam berbohong dengan sang istri dan mengaku bahwa ibunya telah meninggal dunia.
+Suatu hari, Malin dan istrinya terpaksa berlabuh ke pulau tempat kampung halamannya karena cuaca buruk. Istri Malin juga memaksa suaminya untuk turun ke pulau dan membeli ikan. Malin cemas karena dia takut bertemu ibunya.
+Saat dia turun dari kapal, semua orang menyambunya dan menyebutnya 'saudagar kaya'. Tak jauh dari situ, ibu Malin yang kebetulan sedang membantu nelayan, melihat sosok putranya. Ia lalu mendekat untuk memastikannya.
+"Malin...Malin Kundang anakku," kata sang ibu langsung memeluk Malin.
+
+    "Hei, kau wanita tua, diapa kau hingga berani memanggilku anakmu?"
+
+    Istri Malin lalu berusaha menengahi dan meminta sang ibu menunjukkan bukti bahwa Malin adalah anaknya. Ibu Malin pun mengatakan luka di tangan Malin yang telah ada sejak kecil. Istri Malin pun menyadari bahwa ucapan wanita tua di hadapannya memang benar.
+"Suamiku, mengapa kau mengingkari ibumu sendiri?" tanya istri Malin.
+
+    Malin Kundang tak peduli dan tetap tak ingin mengakui ibunya. Sang ibu lalu meratap dan tepat saat itu hujan deras. Tiba-tiba petir menyambar tetap di kaki Malin dan mendadak tubuhnya menjadi kaku seperti batu. Malin amat ketakutan dan dia menyadari telah durhaka dan berdosa pada ibunya.
+"Ibu, tolong ampuni aku. Tolong selamatkan aku," teriak Malin.
+
+    Ibu Malin berusaha menolong tapi terlambat karena anaknya sudah berubah menjadi batu. Dari cerita ini, anak bisa mendapatkan pesan moral untuk menepati janji, serta tidak durhaka kepada orang tua
+    
+    ''',
+                style: TextStyle(fontSize: 15, color: Colors.white),
+              ),
             ),
             alignment: Alignment.center,
-            color: Colors.blue,
+            color: Colors.black87,
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -126,49 +152,56 @@ class _PlayMusicState extends State<PlayMusic> {
               ),
             ],
           ),
-          Text('Audio SpeedX:', style: TextStyle(fontSize: 20),),
+          Text(
+            'Audio SpeedX:',
+            style: TextStyle(fontSize: 20),
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               IconButton(
-                onPressed: (){
+                onPressed: () {
                   setState(() {
                     player.setPlaybackRate(1.0);
                   });
                 },
                 icon: Icon(
                   Icons.looks_one,
-                ),iconSize: 35,
+                ),
+                iconSize: 35,
               ),
               IconButton(
-                onPressed: (){
+                onPressed: () {
                   setState(() {
                     player.setPlaybackRate(2.0);
                   });
                 },
                 icon: Icon(
                   Icons.looks_two,
-                ),iconSize: 35,
+                ),
+                iconSize: 35,
               ),
               IconButton(
-                onPressed: (){
+                onPressed: () {
                   setState(() {
                     player.setPlaybackRate(3.0);
                   });
                 },
                 icon: Icon(
                   Icons.looks_3,
-                ),iconSize: 35,
+                ),
+                iconSize: 35,
               ),
               IconButton(
-                onPressed: (){
+                onPressed: () {
                   setState(() {
                     player.setPlaybackRate(4.0);
                   });
                 },
                 icon: Icon(
                   Icons.looks_4,
-                ),iconSize: 35,
+                ),
+                iconSize: 35,
               )
             ],
           )
